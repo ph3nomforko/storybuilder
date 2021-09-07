a9f0857ed22767b168de74635fb488242ce91f53
Codi Kraus <codi.kraus@gmail.com>
2021-09-07 15:50:36 -0500

    add create for login to sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,14 @@
 class SessionsController < ApplicationController
-    def home
+    
+    def create
+        user = User.find_by(username: params[:user][:username])
+        if user && user.authenticate(params[:user][:password])
+            session[:user_id] = user.id
+            redirect_to user_path(user)
+        else
+            flash[:message] = "Incorrect credentials. Please try again."
+            redirect_to login_path
+        end
     end
 
     def destroy
