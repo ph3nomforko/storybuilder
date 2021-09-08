b11530655fb08bb65d4ead99ed0978e393ec2877
Codi Kraus <codi.kraus@gmail.com>
2021-09-08 10:49:24 -0500

    basic journal controller new/index logic

--- app/controllers/journals_controller.rb
+++ app/controllers/journals_controller.rb
@@ -1,2 +1,32 @@
 class JournalsController < ApplicationController
+        before_action :redirect_if_not_logged_in
+    
+        def index
+            if params[:story_id] && @story = Story.find_by_id(params[:story_id])
+                @journals = @story.journals
+            else
+                flash[:message] = "That story has not been created yet." if params[:story_id]
+                redirect_to new_story_path
+            end
+        end
+    
+        def new
+            @journal = Journal.new
+        end
+    
+        def create
+            @journal = current_user.journals.build(journal_params)
+            if @journal.save
+                redirect_to characters_path
+            else
+                render :new
+            end
+        end
+    
+        private
+    
+            def journal_params
+                params.require(:journal).permit(:content, :story_id)
+            end
+    end
 end
