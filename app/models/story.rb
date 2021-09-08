71074957f340520c90b2054b8f609f584f408a72
Codi Kraus <codi.kraus@gmail.com>
2021-09-08 11:16:52 -0500

    add has many journals to user and story

--- app/models/story.rb
+++ app/models/story.rb
@@ -2,4 +2,6 @@ class Story < ApplicationRecord
   belongs_to :user
   has_many :characters
   has_many :users, through: :characters
+  has_many :journals
+  has_many :users, through: :journals
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
     has_many :stories
     has_many :characters
     has_many :played_stories, through: :characters, source: :story
+    has_many :journals
+    has_many :journaled_stories, through: :journals, source: :story
 
     has_secure_password
 end
