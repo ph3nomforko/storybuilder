1b3075e329d332abeacbb7875cf35164622bb9c4
Codi Kraus <codi.kraus@gmail.com>
2021-09-07 19:39:15 -0500

    create initial characters controller

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -1,2 +1,22 @@
 class CharactersController < ApplicationController
+    before_action :redirect_if_not_logged_in
+
+    def new
+        @character = Character.new
+    end
+
+    def create
+        @character = current_user.characters.build(character_params)
+        if @character.save
+            redirect_to characters_path
+        else
+            render :new
+        end
+    end
+
+    private
+
+        def character_params
+            params.require(:character).permit(:name, :species, :role, :level, :description, :story_id)
+        end
 end
