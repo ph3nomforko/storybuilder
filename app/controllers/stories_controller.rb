deac44f0219f5a4e99a1d4c12085964fc047962d
Codi Kraus <codi.kraus@gmail.com>
2021-09-07 19:09:38 -0500

    finish new,create, and params for stories

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -1,3 +1,23 @@
 class StoriesController < ApplicationController
     before_action :redirect_if_not_logged_in
+
+    def new
+        @story = Story.new
+    end
+
+    def create
+        @story = current_user.stories.build(story_params)
+        if @story.save
+            redirect_to stories_path
+        else
+            render :new
+        end
+    end
+
+    private
+
+        def story_params
+            params.require(:story).permit(:name, :summary, :story_outline)
+        end
+
 end
