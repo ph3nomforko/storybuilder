4d3910edd51b297b43e20f7c51f94aa0add81ab8
Codi Kraus <codi.kraus@gmail.com>
2021-09-08 10:44:11 -0500

    add nested routes to users and stories

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,14 @@ Rails.application.routes.draw do
   delete '/logout' => 'sessions#destroy'
 
   resources :characters
-  resources :users
-  resources :stories
+  resources :journals
+
+  resources :users do
+    resources :stories, only: [:new, :create, :index]
+  end
+
+  resources :stories do
+    resources :characters, only: [:new, :create, :index]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
