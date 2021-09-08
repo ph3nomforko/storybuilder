d4c49f8890a55711cc448efb690a7376d6cb4646
Codi Kraus <codi.kraus@gmail.com>
2021-09-08 11:17:23 -0500

    remove extraneous 'end' from journal controller

--- app/controllers/journals_controller.rb
+++ app/controllers/journals_controller.rb
@@ -1,32 +1,31 @@
 class JournalsController < ApplicationController
-        before_action :redirect_if_not_logged_in
-    
-        def index
-            if params[:story_id] && @story = Story.find_by_id(params[:story_id])
-                @journals = @story.journals
-            else
-                flash[:message] = "That story has not been created yet." if params[:story_id]
-                redirect_to new_story_path
-            end
+    before_action :redirect_if_not_logged_in
+
+    def index
+        if params[:story_id] && @story = Story.find_by_id(params[:story_id])
+            @journals = @story.journals
+        else
+            flash[:message] = "That story has not been created yet." if params[:story_id]
+            redirect_to new_story_path
         end
-    
-        def new
-            @journal = Journal.new
-        end
-    
-        def create
-            @journal = current_user.journals.build(journal_params)
-            if @journal.save
-                redirect_to characters_path
-            else
-                render :new
-            end
-        end
-    
-        private
-    
-            def journal_params
-                params.require(:journal).permit(:content, :story_id)
-            end
     end
+
+    def new
+        @journal = Journal.new
+    end
+
+    def create
+        @journal = current_user.journals.build(journal_params)
+        if @journal.save
+            redirect_to story_path(@journal.story_id)
+        else
+            render :new
+        end
+    end
+
+    private
+
+        def journal_params
+            params.require(:journal).permit(:content, :story_id)
+        end
 end
