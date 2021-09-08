ab55fb01226094b0b273971adf07e1bf5e4d4800
Codi Kraus <codi.kraus@gmail.com>
2021-09-08 10:44:36 -0500

    remove extraneous flash message

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,6 @@ class SessionsController < ApplicationController
             session[:user_id] = user.id
             redirect_to user_path(user)
         else
-            flash[:message] = "Incorrect credentials. Please try again."
             redirect_to login_path
         end
     end
