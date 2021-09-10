37f0507a598405e809077d1a2fe0b3d58c1f6c6a
Codi Kraus <codi.kraus@gmail.com>
2021-09-10 15:25:43 -0500

    add user edit form and logic

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,14 +16,33 @@ class UsersController < ApplicationController
 
     def show
         redirect_if_not_logged_in
-        @user = User.find_by(id: params[:id])
+        set_user
         redirect_to root_path if !@user
+    end
+
+    def edit
+        set_user
+        redirect_to user_path(current_user) if @user.id != current_user.id
+    end
+
+    def update
+        set_user
+        redirect_to user_path(current_user) if @user.id != current_user.id
+        if @user.update(user_params)
+            redirect_to user_path(@user)
+        else
+            render :edit
+        end
     end
 
     private
 
-        def user_params
-            params.require(:user).permit(:username, :email, :password)
-        end
+    def user_params
+        params.require(:user).permit(:username, :email, :password)
+    end
+
+    def set_user
+        @user = User.find_by(id: params[:id])
+    end
 
 end
