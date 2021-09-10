4a652e4109171b1c3974d148e740e7a2822efb65
Codi Kraus <codi.kraus@gmail.com>
2021-09-10 17:37:33 -0500

    add index /redirect to user controller so no error

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,11 @@ class UsersController < ApplicationController
         @user = User.new
     end
 
+    def index
+        redirect_if_not_logged_in
+        redirect_to user_path(current_user)
+    end
+
     def create
         @user = User.new(user_params)
         if @user.save
