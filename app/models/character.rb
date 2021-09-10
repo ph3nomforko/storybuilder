93768746fde20adb27c102b3662b5a9cdac10f00
Codi Kraus <codi.kraus@gmail.com>
2021-09-10 16:43:59 -0500

    add alphabetizing scope method to characters

--- app/models/character.rb
+++ app/models/character.rb
@@ -3,4 +3,6 @@ class Character < ApplicationRecord
   belongs_to :story
 
   validates :name, :species, :role, :level, :description, :passcode, presence: true
+
+  scope :alpha, -> { order (:name) }
 end
